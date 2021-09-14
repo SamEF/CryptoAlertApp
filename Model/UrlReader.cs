5b82ffed79a5d12e687d309999fe1617fe962b72
Sami El Fanneri <samelfanneri@gmail.com>
2021-09-14 12:18:08 -0600

    first push

---
+++ Model/UrlReader.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Text.RegularExpressions;
+using HtmlAgilityPack;
+
+namespace CryptoAlertApp.Model
+{
+     public class UrlReader
+     {
+          private string GetSourceFromURL(string url)
+          {
+               using (WebClient client = new WebClient())
+               {
+                    client.Headers.Add("User-Agent: Other");
+                    string htmlCode = client.DownloadString(url);
+
+                    return htmlCode;
+               }
+          }
+
+          //
+          // Example:
+          // url = www.blog.coinbase.com | node = //a | attribute = href
+          public List<string> GetAllStringNodes(string url, string node, string attribute)
+          {
+               List<string> response = new List<string>();
+               HtmlDocument htmlDoc = new HtmlDocument();
+               htmlDoc.LoadHtml(GetSourceFromURL(url));
+
+               var result = htmlDoc.DocumentNode.SelectNodes(node);
+
+               foreach (var x in result)
+               {    
+                    response.Add(x.Attributes[attribute].Value);
+               }
+
+               return response;
+          }
+
+          //
+          // Gets the text between the start and end tags of the object.
+          // Example:
+          // url = www.blog.coinbase.com | node = //body | attribute = /h1
+          public void GetAllStringInnerHtml(string url, string node, string tag)
+          {
+               List<string> response = new List<string>();
+               HtmlDocument htmlDoc = new HtmlDocument();
+               htmlDoc.LoadHtml(GetSourceFromURL(url));
+
+               var result = htmlDoc.DocumentNode.SelectNodes(node + tag);
+
+               foreach (var x in result)
+               {
+                    //response.Add(x.InnerText);
+                    Console.WriteLine(x.InnerText);
+               }
+
+               //return response;
+          }
+     }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CryptoAlertApp.Model;
+using System;
+
+namespace CryptoAlertApp
+{
+     class Program
+     {
+          static void Main(string[] args)
+          {
+               UrlReader urlReader = new UrlReader();
+
+               urlReader.GetAllStringInnerHtml("https://blog.coinbase.com/", "//body","/h3");
+          }
+     }
+}
